5b3c287db9f4bbe002fdeb56e4f3e5ecfe09af23
hajczek <i.hajczewska@gmail.com>
2019-10-28 10:13:44 +0100

    Added structure of code for this project

--- src/App.js
+++ src/App.js
@@ -4,20 +4,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="drum-machine">
+        <div id="display">
+          <button class="drum-pad" id="btnQ">Q</button>
+          <button class="drum-pad" id="btnW">W</button>
+          <button class="drum-pad" id="btnE">E</button>
+          <button class="drum-pad" id="btnA">A</button>
+          <button class="drum-pad" id="btnS">S</button>
+          <button class="drum-pad" id="btnD">D</button>
+          <button class="drum-pad" id="btnZ">Z</button>
+          <button class="drum-pad" id="btnX">X</button>
+          <button class="drum-pad" id="btnC">C</button>
+        </div>
+      </div>
     </div>
   );
 }
