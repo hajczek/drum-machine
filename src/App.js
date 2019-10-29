15672664012b6d27355a9927545404df50177cd5
hajczek <i.hajczewska@gmail.com>
2019-10-29 17:01:05 +0100

    Changed buttpons to divs

--- src/App.js
+++ src/App.js
@@ -6,15 +6,15 @@ function App() {
     <div className="App">
       <div id="drum-machine">
         <div id="display">
-          <button class="drum-pad" id="btnQ">Q</button>
-          <button class="drum-pad" id="btnW">W</button>
-          <button class="drum-pad" id="btnE">E</button>
-          <button class="drum-pad" id="btnA">A</button>
-          <button class="drum-pad" id="btnS">S</button>
-          <button class="drum-pad" id="btnD">D</button>
-          <button class="drum-pad" id="btnZ">Z</button>
-          <button class="drum-pad" id="btnX">X</button>
-          <button class="drum-pad" id="btnC">C</button>
+          <div class="drum-pad" id="btnQ">Q</div>
+          <div class="drum-pad" id="btnW">W</div>
+          <div class="drum-pad" id="btnE">E</div>
+          <div class="drum-pad" id="btnA">A</div>
+          <div class="drum-pad" id="btnS">S</div>
+          <div class="drum-pad" id="btnD">D</div>
+          <div class="drum-pad" id="btnZ">Z</div>
+          <div class="drum-pad" id="btnX">X</div>
+          <div class="drum-pad" id="btnC">C</div>
         </div>
       </div>
     </div>
