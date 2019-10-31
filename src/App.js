75b9e4c9b92d71caea0045e95b18fd1b24da83ff
hajczek <i.hajczewska@gmail.com>
2019-10-31 08:30:28 +0100

    Added file with sounds

--- src/App.js
+++ src/App.js
@@ -1,62 +1,71 @@
 import React from 'react';
 import './App.css';
+import s1 from './assets/sounds/s1.mp3';
+import s2 from './assets/sounds/s2.mp3';
+import s3 from './assets/sounds/s3.mp3';
+import s4 from './assets/sounds/s4.mp3';
+import s5 from './assets/sounds/s5.mp3';
+import s6 from './assets/sounds/s6.mp3';
+import s7 from './assets/sounds/s7.mp3';
+import s8 from './assets/sounds/s8.mp3';
+import s9 from './assets/sounds/s9.mp3';
 
 function App() {
   return (
     <div className="App">
       <div id="drum-machine">
         <div id="display">
-          <div class="drum-pad">
-            <audio class="clip" id="Q">
-              <source src="" type=""></source>
+          <div className="drum-pad">
+            <audio controls className="clip" id="Q">
+              <source src={s1} type="audio/mpeg"></source>
               Q
             </audio>
           </div>
-          <div class="drum-pad">
-            <audio class="clip" id="W">
-              <source src="" type=""></source>
+          <div className="drum-pad">
+            <audio controls className="clip" id="W">
+              <source src={s2} type="audio/mpeg"></source>
               W
             </audio>
           </div>
-          <div class="drum-pad">
-            <audio class="clip" id="E">
-             <source src="" type=""></source>
+          <div className="drum-pad">
+            <audio controls className="clip" id="E">
+             <source src={s3} type="audio/mpeg"></source>
               E
             </audio>
           </div>
-          <div class="drum-pad">
-            <audio class="clip" id="A">
-              <source src="" type=""></source>
+          <div className="drum-pad">
+            <audio controls className="clip" id="A">
+              <source src={s4} type="audio/mpeg"></source>
               A
             </audio>
           </div>
-          <div class="drum-pad">
-            <audio class="clip" id="S">
-              <source src="" type=""></source>
+          <div className="drum-pad">
+            <audio controls className="clip" id="S">
+              <source src={s5} type="audio/mpeg"></source>
               S
             </audio>
           </div>
-          <div class="drum-pad">
-            <audio class="clip" id="D">
-              <source src="" type=""></source>
+          <div className="drum-pad">
+            <audio controls className="clip" id="D">
+              <source src={s6} type="audio/mpeg"></source>
               D
             </audio>
           </div>
-          <div class="drum-pad">
-            <audio class="clip" id="Z">
-              <source src="" type=""></source>
+          <div className="drum-pad">
+            <audio controls className="clip" id="Z">
+              <source src={s7} type="audio/mpeg"></source>
               Z
             </audio>
           </div>
-          <div class="drum-pad">
-            <audio class="clip" id="X">
-              <source src="" type=""></source>
+          <div className="drum-pad">
+            <audio controls className="clip" id="X">
+              <source src={s8} type="audio/mpeg"></source>
               X
             </audio>
           </div>
-          <div class="drum-pad">
-            <audio class="clip" id="C">
-              <source src="" type=""></source>
+          <div className="drum-pad">
+            <audio controls className="clip" id="C">
+              <source src={s9} type="audio/mpeg"></source>
               C
             </audio>
           </div>
