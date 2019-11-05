4e3d07a304cce726e438a671d939d90b14a73dd9
hajczek <i.hajczewska@gmail.com>
2019-11-05 10:35:03 +0100

    Added click event with testing function for every pads

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import s1 from './assets/sounds/s1.mp3';
 import s2 from './assets/sounds/s2.mp3';
 import s3 from './assets/sounds/s3.mp3';
@@ -9,69 +9,82 @@ import s7 from './assets/sounds/s7.mp3';
 import s8 from './assets/sounds/s8.mp3';
 import s9 from './assets/sounds/s9.mp3';
 
-function App() {
-  return (
-    <div className="App">
-      <div id="drum-machine" className="drums">
-        <div id="display" class="container">
-          <div className="drum-pad">
-            <audio className="clip" id="Q">
-              <source src={s1} type="audio/mpeg"></source>
-            </audio>
-            Q
-          </div>
-          <div className="drum-pad">
-            <audio className="clip" id="W">
-              <source src={s2} type="audio/mpeg"></source>
-            </audio>
-            W
-          </div>
-          <div className="drum-pad">
-            <audio className="clip" id="E">
-             <source src={s3} type="audio/mpeg"></source>
-            </audio>
-            E
-          </div>
-          <div className="drum-pad">
-            <audio className="clip" id="A">
-              <source src={s4} type="audio/mpeg"></source>
-            </audio>
-            A
-          </div>
-          <div className="drum-pad">
-            <audio className="clip" id="S">
-              <source src={s5} type="audio/mpeg"></source>
-            </audio>
-            S
-          </div>
-          <div className="drum-pad">
-            <audio className="clip" id="D">
-              <source src={s6} type="audio/mpeg"></source>
-            </audio>
-            D
-          </div>
-          <div className="drum-pad">
-            <audio className="clip" id="Z">
-              <source src={s7} type="audio/mpeg"></source>
-            </audio>
-            Z
-          </div>
-          <div className="drum-pad">
-            <audio className="clip" id="X">
-              <source src={s8} type="audio/mpeg"></source>
-            </audio>
-            X
-          </div>
-          <div className="drum-pad">
-            <audio className="clip" id="C">
-              <source src={s9} type="audio/mpeg"></source>
-            </audio>
-            C
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      
+    };
+  }
+
+  playSound(){
+    console.log('It was clicked');
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div id="drum-machine" className="drums">
+          <div id="display" className="container">
+            <div className="drum-pad" onClick={this.playSound}>
+              <audio className="clip" id="Q">
+                <source src={s1} type="audio/mpeg"></source>
+              </audio>
+              Q
+            </div>
+            <div className="drum-pad" onClick={this.playSound}>
+              <audio className="clip" id="W">
+                <source src={s2} type="audio/mpeg"></source>
+              </audio>
+              W
+            </div>
+            <div className="drum-pad" onClick={this.playSound}>
+              <audio className="clip" id="E">
+               <source src={s3} type="audio/mpeg"></source>
+              </audio>
+              E
+            </div>
+            <div className="drum-pad" onClick={this.playSound}>
+              <audio className="clip" id="A">
+                <source src={s4} type="audio/mpeg"></source>
+              </audio>
+              A
+            </div>
+            <div className="drum-pad" onClick={this.playSound}>
+              <audio className="clip" id="S">
+                <source src={s5} type="audio/mpeg"></source>
+              </audio>
+              S
+            </div>
+            <div className="drum-pad" onClick={this.playSound}>
+              <audio className="clip" id="D">
+                <source src={s6} type="audio/mpeg"></source>
+              </audio>
+              D
+            </div>
+            <div className="drum-pad" onClick={this.playSound}>
+              <audio className="clip" id="Z">
+                <source src={s7} type="audio/mpeg"></source>
+              </audio>
+              Z
+            </div>
+            <div className="drum-pad" onClick={this.playSound}>
+              <audio className="clip" id="X">
+                <source src={s8} type="audio/mpeg"></source>
+              </audio>
+              X
+            </div>
+            <div className="drum-pad" onClick={this.playSound}>
+              <audio className="clip" id="C">
+                <source src={s9} type="audio/mpeg"></source>
+              </audio>
+              C
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }  
 }
 
 export default App;
