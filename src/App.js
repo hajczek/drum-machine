7fe1ee21897b1a1c58b3c7ee0dbe8888d5746b32
hajczek <i.hajczewska@gmail.com>
2019-11-12 10:43:53 +0100

    Added map function for create all pads

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import s7 from './assets/sounds/s7.mp3';
 import s8 from './assets/sounds/s8.mp3';
 import s9 from './assets/sounds/s9.mp3';
 
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -24,66 +25,20 @@ class App extends Component {
   }
 
   render() {
+    const sounds = ['Q', 'W', 'E', 'A', 'S', 'D', 'Z', 'X', 'C'];
     return (
       <div className="App">
         <h1>DRUM MACHINE PANEL</h1>
         <div id="drum-machine" className="drums">
-
           <div id="display" className="container">
-            <div className="drum-pad" onClick={this.playSound} title="Sound Q">
-              <audio className="clip" id="Q">
-                <source src={s1} type="audio/mpeg"></source>
-              </audio>
-              Q
-            </div>
-            <div className="drum-pad" onClick={this.playSound} title="Sound W">
-              <audio className="clip" id="W">
-                <source src={s2} type="audio/mpeg"></source>
-              </audio>
-              W
-            </div>
-            <div className="drum-pad" onClick={this.playSound} title="Sound E">
-              <audio className="clip" id="E">
-                <source src={s3} type="audio/mpeg"></source>
-              </audio>
-              E
-            </div>
-            <div className="drum-pad" onClick={this.playSound} title="Sound A">
-              <audio className="clip" id="A">
-                <source src={s4} type="audio/mpeg"></source>
-              </audio>
-              A
-            </div>
-            <div className="drum-pad" onClick={this.playSound} title="Sound S">
-              <audio className="clip" id="S">
-                <source src={s5} type="audio/mpeg"></source>
-              </audio>
-              S
-            </div>
-            <div className="drum-pad" onClick={this.playSound} title="Sound D">
-              <audio className="clip" id="D">
-                <source src={s6} type="audio/mpeg"></source>
-              </audio>
-              D
-            </div>
-            <div className="drum-pad" onClick={this.playSound} title="Sound Z">
-              <audio className="clip" id="Z">
-                <source src={s7} type="audio/mpeg"></source>
-              </audio>
-              Z
-            </div>
-            <div className="drum-pad" onClick={this.playSound} title="Sound X">
-              <audio className="clip" id="X">
-                <source src={s8} type="audio/mpeg"></source>
-              </audio>
-              X
-            </div>
-            <div className="drum-pad" onClick={this.playSound} title="Sound C">
-              <audio className="clip" id="C">
-                <source src={s9} type="audio/mpeg"></source>
-              </audio>
-              C
-            </div>
+            {sounds.map((sound, i) => {
+              return <div className="drum-pad" onClick={this.playSound} title={"Sound " + sound} key={i}>
+                <audio className="clip" id={sound}>
+                  <source src={"s" + ++i} type="audio/mpeg"></source>
+                </audio>
+                {sound}
+              </div>;
+            })}
           </div>
           <div className="panel">
             <h2>PANEL</h2>
