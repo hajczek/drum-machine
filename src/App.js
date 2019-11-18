d1c0ef24891cc167a1c23c300cdb43730b2feb14
hajczek <i.hajczewska@gmail.com>
2019-11-18 08:51:55 +0100

    Added functionality for display title of pad after press key

--- src/App.js
+++ src/App.js
@@ -10,11 +10,12 @@ import s8 from "./assets/sounds/s8.mp3";
 import s9 from "./assets/sounds/s9.mp3";
 
 const soundsName = ["Q", "W", "E", "A", "S", "D", "Z", "X", "C"];
-// const charCodes = [81, 87, 69, 65, 83, 68, 90, 88, 67];
 const sounds = [s1, s2, s3, s4, s5, s6, s7, s8, s9];
+const pads = ["Pad 1", "Pad 2", "Pad 3", "Pad 4", "Pad 5", "Pad 6", "Pad 7", "Pad 8", "Pad 9"];
+// const charCodes = [81, 87, 69, 65, 83, 68, 90, 88, 67];
 
-class App extends React.Component {
-  componentDidMount = () => {
+class App extends Component {
+  componentDidMount(e) {
     // Listens for keydown event
     document.onkeydown = e => {
       e = e || window.event;
@@ -24,9 +25,10 @@ class App extends React.Component {
       // Use name of key for find suitable element to active
       let activePad = document.getElementById(pad);
       // Play sound
-      activePad.play();
+      const textField = document.getElementById("text-field");
+      textField.innerHTML = activePad.title;
     };
-  };
+  }
 
   soundPlay(e) {
     // Define an active element
@@ -42,7 +44,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <h1> DRUM MACHINE PANEL </h1>
+        <h1>DRUM MACHINE PANEL</h1>
         <div id="drum-machine" className="drums">
           <div id="display" className="container">
             {sounds.map((sound, i) => {
@@ -53,7 +55,7 @@ class App extends React.Component {
                   title={"Sound " + soundsName[i]}
                   key={i}
                 >
-                  <audio className="clip" id={soundsName[i]}>
+                  <audio className="clip" id={soundsName[i]} title={pads[i]}>
                     <source src={sounds[i]} type="audio/mpeg"></source>
                   </audio>
                   {soundsName[i]}
