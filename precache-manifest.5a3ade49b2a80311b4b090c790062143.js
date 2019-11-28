c5c0656a4b03b3e166ede43e9ec325dd17c6636d
hajczek <i.hajczewska@gmail.com>
2019-11-28 10:37:37 +0100

    Updates

---
+++ precache-manifest.5a3ade49b2a80311b4b090c790062143.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15b5cdb1f29d4488b5a53dd1d711f612",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "8240a2a99be36c5e6a9e",
+    "url": "/drum-machine/static/css/main.19a86833.chunk.css"
+  },
+  {
+    "revision": "b67eb65f47c3622d7b13",
+    "url": "/drum-machine/static/js/2.b33341eb.chunk.js"
+  },
+  {
+    "revision": "8240a2a99be36c5e6a9e",
+    "url": "/drum-machine/static/js/main.5e467187.chunk.js"
+  },
+  {
+    "revision": "f21ce7720cfc5fa2cd84",
+    "url": "/drum-machine/static/js/runtime-main.61d160df.js"
+  },
+  {
+    "revision": "fc46d8354b7633e6c2a79ab7a78093c5",
+    "url": "/drum-machine/static/media/s1.fc46d835.mp3"
+  },
+  {
+    "revision": "1c679e176ec375f6a78b1cbde14212a6",
+    "url": "/drum-machine/static/media/s2.1c679e17.mp3"
+  },
+  {
+    "revision": "b6731b460c1cb3568e3cb5a768696dce",
+    "url": "/drum-machine/static/media/s3.b6731b46.mp3"
+  },
+  {
+    "revision": "7290a33afe3e1302751ff22382249bdb",
+    "url": "/drum-machine/static/media/s4.7290a33a.mp3"
+  },
+  {
+    "revision": "f8c3e6dfb8ae3f3a5e824b0b6754ca4f",
+    "url": "/drum-machine/static/media/s5.f8c3e6df.mp3"
+  },
+  {
+    "revision": "1d33f90ddf1bb4d04e22b02644a67689",
+    "url": "/drum-machine/static/media/s6.1d33f90d.mp3"
+  },
+  {
+    "revision": "59f9d53f520e4a1773b2eaf89977c8e3",
+    "url": "/drum-machine/static/media/s7.59f9d53f.mp3"
+  },
+  {
+    "revision": "e55cab0094e45fa678369ebcf1304160",
+    "url": "/drum-machine/static/media/s8.e55cab00.mp3"
+  },
+  {
+    "revision": "27d8ea2e58c39de0c322d475394d2826",
+    "url": "/drum-machine/static/media/s9.27d8ea2e.mp3"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/hajczek/drum-machine/precache-manifest.d63223bc3831e19f47443ef8292933c0.js"
+  "/drum-machine/precache-manifest.5a3ade49b2a80311b4b090c790062143.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hajczek/drum-machine/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/drum-machine/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
